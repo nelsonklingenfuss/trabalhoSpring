9e845919de37891dae505716e03e9bd0ef848796
NELSON BASILIO KLINGENFUSS <nelson.klingenfuss@univille.br>
2018-06-06 20:09:43 -0300

    Atualizacao

---
+++ src/main/java/br/univille/projdacs2018/ProjDacs2018Application.java
@@ -0,0 +1,12 @@
+package br.univille.projdacs2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjDacs2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjDacs2018Application.class, args);
+	}
+}
---
+++ src/main/java/br/univille/projdacs2018/controller/HomeController.java
@@ -0,0 +1,14 @@
+package br.univille.projdacs2018.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "Eu não acredito";
+	}
+}
